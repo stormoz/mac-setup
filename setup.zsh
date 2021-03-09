8e68101066b0cc44256854c43efb57482b244bff
Gokulnath Reddy <yrgokulnath@kpmg.com.au>
2021-03-09 22:09:46 +1100

    add dotfiles

--- setup.zsh
+++ setup.zsh
@@ -2,6 +2,15 @@
 
 echo "Setting up a new mac book"
 
+DOTFILES_DIR=$HOME/.dotfiles
+
+if [ -d "$DOTFILES_DIR" ]
+then
+    echo "Directory /path/to/dir exists."
+else
+    echo "Error: Directory /path/to/dir does not exists."
+fi
+
 # Check if xcode-select —-install is installed
 
 # Supporting Functions
@@ -71,11 +80,13 @@ APPLICATIONS=(
   brave-browser
   dbeaver-community
   docker
+  dotnet-sdk
   drawio
   github
   google-chrome
   iterm2
   kdiff3
+  lastpass
   lens
   microsoft-azure-storage-explorer
   pgadmin4
@@ -101,3 +112,24 @@ done
 brew cleanup
 
 echo "Hombrew installation complete"
+
+mkdir -p ~/Work/Code # Work Code
+mkdir -p ~/Code # Personal Code
+
+
+echo "Setting up dotfiles"
+
+if [ -d "$DOTFILES_DIR" ]
+then
+  echo "Dot files already exists, pulling in latest changes"
+  cd $DOTFILES_DIR
+  git pull origin master
+  cd $HOME
+else
+  echo "Dotfiles does not exists, installing"
+  git clone https://github.com/ygnr/dotfiles.git ~/.dotfiles
+fi
+
+cd $DOTFILES_DIR
+script/bootstrap
+script/install
